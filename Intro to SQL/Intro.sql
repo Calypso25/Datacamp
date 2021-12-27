b9bcd837cf1d2d517d2bed920ac16b476a1f3fda
Gabriel Pasca <15068940+Calypso25@users.noreply.github.com>
2021-12-27 11:03:36 +0100

    new uppdate

--- Intro to SQL/Intro.sql
+++ Intro to SQL/Intro.sql
@@ -5,3 +5,7 @@ FROM films;
 /* second exercise */
 SELECT title, duration / 60.0 as duration_hours
 FROM films;
+
+/* Third exercise */
+SELECT AVG(duration) / 60.0 as avg_duration_hours
+FROM films;
